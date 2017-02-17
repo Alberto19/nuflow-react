e5f853981fa79dc96ecae83b1881d5462490e499
Junior <jose.farias@itst.com.br>
2017-02-17 16:38:21 -0200

    mudando para material design light

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-import SideBar from './components/sidebar';
-import Search from './components/search';
+import { Layout, Header, Navigation, Drawer, Content, Cell} from 'react-mdl';
+// import SideBar from './components/sidebar';
+// import Search from './components/search';
 
 
 export default class App extends Component {
 
  render() {
     return (
-      <div className="container-fluid">
-      <header className="header row">
-        <div className="col-xs-2">
-          <SideBar/>
-        </div>
-          <div className="col-xs-6 col-lg-7  inputSearch">
-          <Search/>
-          </div>
-      </header>
-        <div className="secondary-navbar row">
-          <div className="secondary-navbar-inner col"></div>
-        </div>
-      </div>
+<div>
+    <Layout fixedHeader>
+        <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
+            <Navigation>
+                <a href="">Link</a>
+                <a href="">Link</a>
+                <a href="">Link</a>
+                <a href="">Link</a>
+            </Navigation>
+        </Header>
+        <Cell hideDesktop={true}>
+        <Drawer title="Title" >
+            <Navigation>
+                <a href="">Link</a>
+                <a href="">Link</a>
+                <a href="">Link</a>
+                <a href="">Link</a>
+            </Navigation>
+        </Drawer>
+        </Cell>
+        <Content />
+    </Layout>
+</div>
     );
   }
 }
 
+      // <div className="container">
+      // <header className="header row">
+      //   <div className="col-xs-2">
+      //     <SideBar/>
+      //   </div>
+      //     <div className="col-xs-6 col-lg-7  inputSearch">
+      //     <Search/>
+      //     </div>
+      // </header>
+      //   <div className="secondary-navbar row">
+      //     <div className="secondary-navbar-inner col"></div>
+      //   </div>
+      // </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './style/app.css';
-import './style/bootstrap.min.css';
-import 'antd/dist/antd.css';
+// import './style/app.css';
+// import './style/bootstrap.min.css';
+// import 'antd/dist/antd.css';
 import 'material-design-icons';
+import 'react-mdl/extra/css/material.blue-deep_orange.min.css';
+import 'react-mdl/extra/material-grid.min.css';
+import 'react-mdl/extra/material.min.js';
+// \node_modules\material-design-lite\dist\material.deep_orange-green.min.css
 
 ReactDOM.render(
 
