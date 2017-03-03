3736036c2c42c6d68dd0b332a2dd5f04ffd9a9ac
Alberto19 <junioralberto19@gmail.com>
2017-03-03 00:19:41 -0300

    populando card com dados reais

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-// import service from '../services/service';
 import axios from 'axios';
-import {Grid, Cell, IconButton} from 'react-mdl';
+import CardFeed from './CardFeed';
 
 
 export default class Feed extends Component{
@@ -12,11 +11,10 @@ export default class Feed extends Component{
 		};
 		this.location = [];
 		this.keyword = '';
-		this.radius = 900;
+		this.radius = 1000;
 
 	}
 	
-
 	componentDidMount(){
 		   if (navigator.geolocation) {
         		navigator.geolocation.getCurrentPosition((position)=>{
@@ -29,6 +27,7 @@ export default class Feed extends Component{
 					}
 			
 					axios.post('https://nuflow.herokuapp.com/search/places', find).then((result)=>{
+						this.setState({lista: result.data});
 							console.log(result.data);
 						}).catch((err)=>{
 							alert(err);
@@ -37,32 +36,11 @@ export default class Feed extends Component{
     		} else { 
         		alert("Geolocation is not supported by this browser.");
     		}
-			
 	}
 
 	render(){
 		return(
-		<Grid className="section--center">
-			<Cell  offsetDesktop={3} col={6} offsetTablet={1} tablet={6} phone={5} >
-				<Grid shadow={2} style={{padding: '0px', height: '200px'}}>
-					<Cell col={5}  tablet={3} phone={2}>
-						<img alt="Nova itapevi" src="https://lh5.googleusercontent.com/-3NGDhv1L-eM/Vp7HaekAjiI/AAAAAAAAAAM/gQNvaq27n8IzCa0XsyCJdWtgxdjz43zlwCJkC/s1600-w2000-h2000/"  style={{ height: '100%', width: '100%' }} />
-					</Cell>
-					<Cell col={7}  tablet={5} phone={2}>
-					<Grid style={{padding: '0px', height: '200px'}}>
-						<Cell col={10}  tablet={5} phone={3}>
-						<p style={{fontSize:'23px', fontWeight:'600',  color:'rgb(33,150,243)', textAlign: 'center' }}>Celeiro da Granja</p>
-						</Cell>
-					<Cell col={2}  tablet={3} phone={1}>
-						<IconButton name="share" />
-					</Cell>
-					</Grid>
-					</Cell>
-				</Grid>
-
-			</Cell>
-		</Grid>
-
+			<CardFeed places={this.state.lista}/>
 		)
 	}
 }
